2b8bac172f8f9e13de8312400ccce3492ec089d6
lixiaolong <1241181400@qq.com>
2025-05-05 19:46:00 +0800

    feat(package): 新增多个依赖并升级 zod 版本
    - 新增 @hookform/resolvers、@radix-ui/react-alert-dialog、@radix-ui/react-label、next-themes、react-hook-form、sonner 和 @types/jsonwebtoken 依赖
    - 升级 zod 依赖至 3.24.4 版本
    - 在布局中添加 Toaster 组件

---
+++ src/app/account/page.tsx
@@ -0,0 +1,22 @@
+import NotAccount from '@/components/NotAccount'
+import Account from '@/components/Account'
+import { authAction } from '@/actions/users'
+import { addressesAction } from '@/actions/addresses'
+import { Address } from '@/types/global'
+
+export default async function Page() {
+	const auth = await authAction()
+	const addresses = await addressesAction(auth.data?.userid)
+	return (
+		<>
+			{auth.status === 200 && auth.data ? (
+				<Account
+					authData={auth.data}
+					addressesData={addresses.data as Address[]}
+				/>
+			) : (
+				<NotAccount />
+			)}
+		</>
+	)
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Toaster } from '@/components/ui/sonner'
 
 export const metadata: Metadata = {
   title: "Next App",
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
